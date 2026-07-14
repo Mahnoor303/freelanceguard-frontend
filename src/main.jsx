56ad572113cbebcb5d1f5e4e3cc9161c5c75dd0a
mahnoordeveloperrr-alt <mahnoor.mughal.developerrr@gmail.com>
2026-07-14 18:40:34 +0500

    Fix Gemini model name + improved dummy

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
@@ -16,7 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <BrowserRouter basename={basename}>
+      <HashRouter>
         <App />
+      </HashRouter>
       </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
