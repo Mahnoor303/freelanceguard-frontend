9b1d672839e358252b39e60a3f0788cd12ff316f
mahnoordeveloperrr-alt <mahnoor.mughal.developerrr@gmail.com>
2026-07-14 18:52:02 +0500

    Fix Gemini model name + improved dummy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
@@ -7,19 +6,11 @@ import i18n from './i18n';
 import App from './App.jsx';
 import './index.css';
 
-// ===== Redirect if URL doesn't start with basename =====
-const basename = '/freelanceguard-frontend';
-if (!window.location.pathname.startsWith(basename)) {
-  window.location.replace(basename + window.location.pathname + window.location.search + window.location.hash);
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={basename}>
-      <HashRouter>
+      <BrowserRouter basename="/freelanceguard-frontend">
         <App />
-      </HashRouter>
       </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
